79bcb5330d98e61165c72bedc424bc9466f51bdc
zevalicious <150071728+zevalicious@users.noreply.github.com>
2023-11-09 12:41:44 -0700

    made a new rocket using if and imputs

---
+++ rocketv5.py
@@ -0,0 +1,59 @@
+
+#zevical, 11/7/2023
+#rocketship1.py
+#very cool rocket text drawing thingy
+#not copyrighted !!
+#imput tester
+print("how large do you want your rocket to be")
+size = int(input())
+if (size>50):
+    size = 50
+
+
+#size cannot = numbers that lead to text wrapping around the screen (for me its high 40's)
+
+def upjet():
+    for i in range(1, size+1):
+        print('|'
+            + "." * (size-i)
+            + "/\\" * (i)
+            + ".." * ((size-i))
+            + "/\\" * (i)
+            + "." * (size-i)
+            + "|")
+def downjet():
+    for i in range(1, size+1):
+        print('|'
+            + "." * (i-1)
+            + "\\/" * ((size-i)+1)
+            + ".." * (i-1)
+            + "\\/" * ((size-i)+1)
+            + "." * (i-1)
+            + "|")
+def split():
+        print(("+")+("=*"* (size*2))+("+"))
+#jetfuel issue fixed 
+def jetfuel():
+    for i in range(1, (size*2)+1):
+        print(
+            " " * ((size*2)-i)
+            + "/" * (i)
+            + "**"
+            + "\\" * (i))
+
+def diamondbody():
+    upjet()
+    downjet()
+def hourglassbody():
+    downjet()
+    upjet()
+    
+
+#code
+jetfuel()
+split()
+diamondbody()
+split()
+hourglassbody()
+split()
+jetfuel()
