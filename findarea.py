86cc72e4e1765c9ab767dabdc10d4bb163d7df39
zev <150071728+zevalicious@users.noreply.github.com>
2023-11-17 11:23:02 -0700

    Update findarea.py using caht gpit

--- findarea.py
+++ findarea.py
@@ -2,18 +2,22 @@
 ans=2
 shape=(input('''Imput your variables for:generallized quadrilateral, rectangle, triangle, parallelagram, or trapazoid to calculate area'''))
 
-if "gen" or "quad" in shape.lower():
-    print('''area of a quadrilateral is calculated by seperating the quadrilateral into two triangles split using the diagonal of the quadrilateral''')
-    diagonal=float(input("enter diagonal of the quadrilateral"))
-    height1=float(input("enter height of triangle 1"))
-    height2=float(input("enter height of triangle 2"))
-    if diagonal>0 and height1>0 and height2>0:
-        print("the volume of the generalized quadrilateral with a diagonal of", diagonal, "is", ((1/2) * (diagonal)) * (height1 + height2))
-    while diagonal<0 or height1<0 or height2<0:
-        print("values must be a positive real numbers")
-        height1=(input("enter new height of triangle 1"))
-        height2=(input("enter new height of triangle 2"))
-        diaginal=(input("enter new length of diagonal"))
+
+if "gen" in shape.lower() or "quad" in shape.lower():
+    print('''The area of a quadrilateral is calculated by separating the quadrilateral into two triangles split using the diagonal of the quadrilateral.''')
+    
+    while True:
+        diagonal = float(input("Enter diagonal of the quadrilateral: "))
+        height1 = float(input("Enter height of triangle 1: "))
+        height2 = float(input("Enter height of triangle 2: "))
+        
+        if diagonal > 0 and height1 > 0 and height2 > 0:
+            area = 0.5 * diagonal * (height1 + height2)
+            print(f"The area of the generalized quadrilateral with a diagonal of {diagonal} is {area}")
+            break  # Break out of the loop if valid inputs are provided
+        else:
+            print("Values must be positive real numbers. Please enter valid inputs.")
+
 
 if "rect" or "par" or "squ" in shape.lower():
     height=float(input("enter height or altitude"))
