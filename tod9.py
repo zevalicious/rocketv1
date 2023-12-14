aaa4f4fd102366e7b77e60d8589c9337629a4cc0
zevalicious <150071728+zevalicious@users.noreply.github.com>
2023-12-14 11:49:12 -0700

    Update tod9.py

--- tod9.py
+++ tod9.py
@@ -6,16 +6,23 @@ answer = [random.randint(0, 9), random.randint(0, 9),
           random.randint(0, 9), random.randint(0, 9)]
 
 while x != "yes":
-    x = input("enter your 4 number answer")
-    x.split()
-    for i in range(0,3):
+    x = [int(input("enter your 1st number"
+                   )), int(input("""enter your 2nd number"""
+                                 )), int(input("enter your 3rd number"
+                                               )), int(input("enter your 4th number"))]
+    print(x)
+    for i in range(4):
         if x[i] == answer[i]:
-            print(f"{x[i]} is green")
-        for x[i] in answer[:]:
-            print(f'{x[i]} is yellow')
+            print(x[i])
+            print(f"{i+1} is green")
+        elif x[i] in answer:
+            print(x[i])
+            print(f'{i+1} is yellow')
         else:
-            print("red")
-        if x[0] == answer[0] and x[1] == answer[1] and x[2] == answer[2] and x[3] == answer[3] and x[4] == answer[4]:
+            print(x[i])
+            print(f"{i+1} is red")
+        print(x)
+        if x == answer:
             x = "yes"
     
 if x == answer:
