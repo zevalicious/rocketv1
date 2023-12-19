14a42f37c8fbe529cc63399b40d8c0f77770dcca
zevalicious <150071728+zevalicious@users.noreply.github.com>
2023-12-19 11:24:21 -0700

    Create final1day.py

---
+++ final1day.py
@@ -0,0 +1,8 @@
+rollio = input("input your dice roll(s) within the format (3d5+-3)")
+d=rollio.index(d)
+number=int(rollio[:d])
+length = len(rollio)
+plus=index
+bonus=rollio[+:]
+yes=rollio[d+1:+]
+roll={}
